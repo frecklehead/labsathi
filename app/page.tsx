6093c58315d494f6b9eecb2fda9dd2e09da08ecb
sostika <sostika002@gmail.com>
2026-01-02 23:44:57 +0545

    created a test tube

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
 import Image from "next/image";
+import { Tube } from "./components/lab/Tube";
 
 export default function Home() {
   return (
-   <>
-    Labsathi
-   </>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center", // horizontal
+        alignItems: "center",     // vertical
+        height: "100vh",          // full viewport height
+      }}
+    >
+      <Tube />
+    </div>
   );
 }
